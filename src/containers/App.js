49924e2048d871b8ed725600278b376ead631a85
wta666 <wtaz666@163.com>
2018-11-21 13:56:25 +0800

    react ok

---
+++ src/containers/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import "common/css/style.css";
+
+
+class App extends Component {
+  render() {
+    return  <div></div>
+    
+  }
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import App from 'view/App';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<App></App>,document.getElementById('root'));
